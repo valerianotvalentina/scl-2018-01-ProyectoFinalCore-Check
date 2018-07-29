56f499adf628f9424ea6ef40c9e320614f1afb0a
MLoretto <loreto.henriquez.m@gmail.com>
2018-07-29 19:21:46 -0400

    Captura Foto Registro

---
+++ src/js/picture.js
@@ -0,0 +1,69 @@
+const constraints = {
+    video: true
+  };
+  const video = document.getElementById('Video');
+  const captureVideoButton =  document.getElementById('btnCaptura');
+  const screenshotButton = document.getElementById('capturePicture');
+  const enableVideo = document.getElementById('enableVideo');
+  const img = document.getElementById('imgFoto');
+  const canvas = document.createElement('canvas');
+
+  navigator.mediaDevices.getUserMedia(constraints).
+    then(handleSuccess).catch(handleError);
+  
+  img.style.display = 'none';
+  enableVideo.style.display = 'none';
+
+
+  enableVideo.onclick = function() {
+    video.style.display = 'block';
+    screenshotButton.style.display = 'block';
+    img.style.display = 'none';
+    enableVideo.style.display = 'none';    
+  }
+
+  screenshotButton.onclick = function() {
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      canvas.getContext('2d').drawImage(video, 0, 0);
+      // Other browsers will fall back to image/png
+      img.src = canvas.toDataURL('image/webp');
+      
+      video.style.display = 'none';
+      screenshotButton.style.display = 'none';
+      img.style.display = 'block';
+      enableVideo.style.display = 'block';
+  };
+
+  function handleSuccess(stream) {
+      screenshotButton.disabled = false;
+      video.srcObject = stream;
+  }
+
+  function handleError(error) {
+      console.error('Error: ', error);
+  }
+
+
+  function subirArchivo(archivo, nombre) {
+    let storageService = firebase.storage();
+    // creo una referencia al lugar donde guardaremos el archivo
+    let refStorage = storageService.ref('userImages').child(currentUser.uid + archivo.name);
+    // Comienzo la tarea de upload
+    const uploadTask = refStorage.put(archivo);
+    // defino un evento para saber qué pasa con ese upload iniciado
+    uploadTask.on('state_changed', null,
+        function(error){
+            console.log('Error al subir el archivo', error);
+        },
+        function(){
+            //obtiene la url de la imagen recien subida
+            uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+                //cambia el source de la imagen por la url de la imagen recien subida
+                //document.getElementById('imgReview').src = downloadURL;
+                console.log()
+              });
+        }
+    );
+}
+
--- src/js/registro.js
+++ src/js/registro.js
@@ -2,18 +2,24 @@ function saveUser(){
     console.log("saveUser");
     let newUser = getForm();
     console.log(newUser);
-    let newRegistroKey = firebase.database().ref().child('registros').push().key;
-    console.log(newRegistroKey);
-    firebase.database().ref(`registros/${newRegistroKey}`).set({
-        userName : newUser.userName,
-        eMail : newUser.eMail,
-        enterpriseFrom : newUser.enterpriseFrom,
-        collaboratorName : newUser.collaboratorName,
-        reasonVisit : newUser.reasonVisit
-    }, function(error){
-        console.log('Listo!!!!');
-       //window.location = 'otro.html';
-   });  
+    if(validateform(newUser) === true){
+        let newRegistroKey = firebase.database().ref().child('registros').push().key;
+        console.log(newRegistroKey);
+        firebase.database().ref(`registros/${newRegistroKey}`).set({
+            userName : newUser.userName,
+            rutUser : newUser.rutUser,
+            eMail : newUser.eMail,
+            enterpriseFrom : newUser.enterpriseFrom,
+            patenteUser : newUser.patenteUser,
+            collaboratorName : newUser.collaboratorName,
+            reasonVisit : newUser.reasonVisit
+        }, function(error){
+            //console.log('Listo!!!!');
+           window.location = 'picture.html';
+       });  
+    }else{
+        console.log('Faltan campos importantes!!!');
+    }
 }
 function getForm(){
   let reasonVisit;
@@ -29,16 +35,57 @@ function getForm(){
   if(document.getElementById("otherCheck").checked === true) {
     reasonVisit = 'other';
   }
-
   let newUser = {
      userName : document.getElementById("userName").value,
+     rutUser : document.getElementById("rutUser").value,
      eMail : document.getElementById("eMail").value,
      enterpriseFrom : document.getElementById("enterpriseFrom").value,
+     patenteUser : document.getElementById("patenteUser").value,
      collaboratorName : document.getElementById("collaboratorName").value,
      reasonVisit : reasonVisit
   } 
   return newUser;
 }
-function validateform(user){
-
+function validateform(user){ //Valida que los campos obligatorios estén completados.
+  let sValid = true;
+  if(user.userName === ''){
+    sValid = false;
+  }
+  if(user.rutUser === ''){
+    sValid = false;
+  }
+  if(user.eMail === '' ){
+    sValid = false;
+  }
+  if(!validarEmail(user.eMail)){
+    sValid = false;
+  }
+  if(user.enterpriseFrom === ''){
+    sValid = false;
+  }
+  if(user.collaboratorName === ''){
+    sValid = false;
+  }
+  if(user.reasonVisit === ''){
+    sValid = false;
+  }
+  let opciones = document.getElementsByName("reasonVisit");//Valida que al menos un check este seleccionado.
+  let seleccionado = false;
+  for(let i=0; i<opciones.length; i++) {    
+    if(opciones[i].checked) {
+      seleccionado = true;
+      break;
+    }
+  }
+  if(!seleccionado) {
+    sValid = false;
+  }
+  return sValid;
 }
+function validarEmail(valor) {// Valida que mail cumpla con formato
+    if (/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(valor)){
+        return true;
+    } else {
+        return false;
+    }
+} 
