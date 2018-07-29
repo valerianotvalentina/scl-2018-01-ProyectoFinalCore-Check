4f7587f68e879368462a56713801e8fcdd45c1db
MLoretto <loreto.henriquez.m@gmail.com>
2018-07-29 14:44:03 -0400

    Registro visita con Firebase OK

--- src/js/registro.js
+++ src/js/registro.js
@@ -1,21 +1,44 @@
 function saveUser(){
-    let newUser = getForm();
     console.log("saveUser");
+    let newUser = getForm();
+    console.log(newUser);
+    let newRegistroKey = firebase.database().ref().child('registros').push().key;
+    console.log(newRegistroKey);
+    firebase.database().ref(`registros/${newRegistroKey}`).set({
+        userName : newUser.userName,
+        eMail : newUser.eMail,
+        enterpriseFrom : newUser.enterpriseFrom,
+        collaboratorName : newUser.collaboratorName,
+        reasonVisit : newUser.reasonVisit
+    }, function(error){
+        console.log('Listo!!!!');
+       //window.location = 'otro.html';
+   });  
 }
 function getForm(){
+  let reasonVisit;
+  if(document.getElementById("meetingCheck").checked === true) {
+    reasonVisit = 'meeting';
+  }
+  if(document.getElementById("interviewCheck").checked === true) {
+    reasonVisit = 'interview';
+  }
+  if(document.getElementById("personalCheck").checked === true) {
+    reasonVisit = 'personal';
+  }
+  if(document.getElementById("otherCheck").checked === true) {
+    reasonVisit = 'other';
+  }
+
   let newUser = {
-     userName = document.getElementById("userName").Value,
-     eMail = document.getElementById("eMail").Value,
-     enterpriseFrom = document.getElementById("enterpriseFrom").Value,
-     collaboratorName = document.getElementById("collaboratorName").Value,
-     meetingCheck = document.getElementById("meetingCheck").Value,
-     interviewCheck = document.getElementById("interviewCheck").Value,
-     personalCheck = document.getElementById("personalCheck").Value,
-     otherCheck = document.getElementById("otherCheck").Value
+     userName : document.getElementById("userName").value,
+     eMail : document.getElementById("eMail").value,
+     enterpriseFrom : document.getElementById("enterpriseFrom").value,
+     collaboratorName : document.getElementById("collaboratorName").value,
+     reasonVisit : reasonVisit
   } 
   return newUser;
 }
 function validateform(user){
-
 
 }
