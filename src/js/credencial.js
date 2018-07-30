7f774c41bb249fa5d307f59e7f45e2f33931fec1
MLoretto <loreto.henriquez.m@gmail.com>
2018-07-30 15:26:11 -0400

    Modificación QR

--- src/js/credencial.js
+++ src/js/credencial.js
@@ -3,13 +3,13 @@ function getData(){ //Obtiene datos de Firebase con KEY
     localStorage.removeItem("registerKey");//Impide reutilización, borrando del almacenamiento local la key
     firebase.database().ref(`registros/${key}`).once('value').then(function(user) {// Obtiene los datos de Firebase con key
         var qrcode = new QRCode(document.getElementById("userQR"), {// Crea objeto Qrcode en el div UserQR
-            width : 120,
-            height : 120
+            width : 120,//ancho del QR
+            height : 120//alto del QR
         });
         document.getElementById('userName').innerText = user.val().userName;
         document.getElementById('imgUser').src = user.val().urlPicture;
-        qrcode.makeCode('{id:"' + key + '", visita:"' + user.val().collaboratorName + '", fecha:"' + user.val().createTime + '"}');//Genera QR
-        console.log('usuario:' + '{id:"' + key + '", visita:"' + user.val().collaboratorName + '", fecha:"' + user.val().createTime + '"}');
+        //qrcode.makeCode('{id:"' + key + '"}');//Genera QR json
+        qrcode.makeCode(key);//Genera QR solo id
         document.getElementById('userDateTime').innerText = user.val().createTime;
         document.getElementById('notificado').innerText = 'Hemos notificado a '+ user.val().collaboratorName +' de tu visita. Espera que venga por ti.';
     });
